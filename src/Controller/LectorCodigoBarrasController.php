ac4c8d63c11f2bac6650baa6089ff1e949c913a6
asortigosa <asortigosa@cistec.es>
2022-11-25 09:08:49 +0100

    Subimos proyecto a repositorio github

---
+++ src/Controller/LectorCodigoBarrasController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LectorCodigoBarrasController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->render('lector_codigo_barras/index.html.twig', [
+            'controller_name' => 'LectorCodigoBarrasController',
+        ]);
+    }
+
+    #[Route('/leer/codigos', name: 'app_leer_codigos')]
+    public function leerCodigosBarras(): Response
+    {
+        $data = $_POST;
+        return $this->redirectToRoute('app_index');
+    }
+}
+
